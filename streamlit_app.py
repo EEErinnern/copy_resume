687a2ea62589a105a31766ff5c8cc25dabc37719
EEErinnern <97386435+EEErinnern@users.noreply.github.com>
2024-04-11 18:22:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,13 +21,6 @@ st.set_page_config(page_title='mehul gupta\'s portfolio' ,layout="wide",page_ico
 
 st.subheader('About me')
 st.write(info['Brief'])
-st.subheader('Career snapshot')
-  
-with st.spinner(text="Building line"):
-    with open('timeline.json', "r") as f:
-        data = f.read()
-        timeline(data, height=500)
-
 
 st.subheader('Skills & Tools ⚒️')
 def skill_tab():
@@ -47,7 +40,6 @@ with st.spinner(text="Loading section..."):
 
 
 st.subheader('Education 📖')
-
 fig = go.Figure(data=[go.Table(
     header=dict(values=list(info['edu'].columns),
                 fill_color='paleturquoise',
@@ -57,27 +49,7 @@ fig = go.Figure(data=[go.Table(
                align='left',height=40,font_size=15))])
 
 fig.update_layout(width=750, height=400)
-st.plotly_chart(fig)
 
 st.subheader('Achievements 🥇')
 achievement_list = ''.join(['<li>'+item+'</li>' for item in info['achievements']])
 st.markdown('<ul>'+achievement_list+'</ul>',unsafe_allow_html=True)
-
-
-st.subheader('Medium Profile ✍️')
-st.markdown("""<a href={}> access full profile here</a>""".format(info['Medium']),unsafe_allow_html=True)
-
-try:
-        page1,page2 = requests.get(info['Medium']), requests.get(info['publication_url'])
-        
-        followers = re.findall('(\d+\.\d+[kK]?) Followers',page1.text)[0]
-        pub_followers = re.findall('Followers (?:\w+\s+){4}(\d+)',re.sub('\W+',' ', page2.text ))[0]
-        
-        cols = st.columns(2)
-        cols[0].metric('Followers',followers)
-        cols[1].metric('Publication followers',pub_followers)
-except:
-    pass
-
-with st.expander('read my latest blogs below'):
-    components.html(embed_component['medium'],height=500)
